97a24d9c0da8ffe2bc35d590197471c56284b06b
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2024-01-03 14:40:01 -0500

    entire project
    the source code for the portfolio

---
+++ script/script.js
@@ -0,0 +1,128 @@
+
+
+/* for side menu */
+
+function showSidebar(){
+
+    
+    document.querySelector('.sidebar').style.display= "flex";
+    document.querySelector('.hamburger-menu').style.color ="transparent";
+    
+}
+
+function hideSidebar(){
+
+    document.querySelector('.sidebar').style.display= "none";
+    document.querySelector('.hamburger-menu').style.color = "white";      
+        
+}
+
+function checkScreenSize(){
+    const screenSize = window.innerWidth;
+    if(screenSize < 900){
+        hideSidebar();
+    }
+
+}
+
+window.addEventListener('resize', checkScreenSize)
+
+/* ----------- code from the office -------------------------*/
+
+var sections = document.querySelectorAll('section');
+var navLinks = document.querySelectorAll('header nav a');
+var contactform = document.querySelector(".contact"); 
+var gtkm = document.querySelector(".about-GetToKnowMe");
+var absk = document.querySelector(".about-MySkills");
+           
+window.onscroll = () => {
+    sections.forEach( sec => { 
+    let top =window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
+    if(top >= offset && top < offset +height){
+        navLinks.forEach(links => {
+            if (links.href.endsWith("#" + id)) { // check if the link matches the section id
+                links.classList.add('active'); // add the active class to the matching link
+            } else {
+                links.classList.remove('active'); // remove the active class from the other links
+            }
+            if(id === "Contact"){
+                DisplayContactForm();    
+            }
+             
+           if (id === 'About') 
+           DisplayAboutContent();
+           else if(id != 'About')
+              HideAboutContent();
+          
+            
+        });
+    }
+     else   if(top + window.innerHeight >= offset && top < offset + height){ // modified condition
+        navLinks.forEach(links => {
+         
+           if (id === 'About') 
+              DisplayAboutContent();
+          
+          
+          
+          
+            
+           else if(id === "Contact")
+              DisplayContactForm();
+            
+            
+           else if(id != "Contact")
+              HideContactForm();
+            
+            
+        });
+    
+    
+    }
+    
+});
+}
+/*  for the About section transition */
+function DisplayAboutContent(){
+        
+        gtkm.style.opacity = 1;
+        gtkm.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)";
+        gtkm.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s";
+        absk.style.visibility = "visible";
+        absk.style.opacity = 1;
+        absk.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)";
+        absk.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s";
+    
+}
+function HideAboutContent(){
+   
+       gtkm.style.visibility = "visible";
+       gtkm.style.opacity = 0;
+       gtkm.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, -80, 0, 0, 1)";
+       gtkm.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0s";
+
+       absk.style.visibility = "visible";
+       absk.style.opacity = 0;
+       absk.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 80, 0, 0, 1)";
+       absk.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0s";
+}
+
+
+
+
+/* for the contact form */
+function DisplayContactForm(){
+      contactform.style.visibility = "visible";
+      contactform.style.opacity = "1";
+      contactform.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)";
+      contactform.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0.2s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0.2s";
+}
+function HideContactForm(){
+      contactform.style.visibility = "visible";
+      contactform.style.opacity = "0";
+      contactform.style.transform = "matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, -80, 0, 1)";
+      contactform.style.transition = "opacity 2s cubic-bezier(0.5, 0, 0, 1) 0s, transform 2s cubic-bezier(0.5, 0, 0, 1) 0s";
+}
