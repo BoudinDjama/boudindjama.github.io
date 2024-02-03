ab860f75ff4cd0a3458a02414f268b6c7ed60654
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2024-02-03 18:00:00 -0500

    more contact test
    testing

--- script/script.js
+++ script/script.js
@@ -290,7 +290,21 @@ const form = document.forms['contact-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(response => { const submittedPopup = document.createElement("div");
+
+  submittedPopup.innerHTML = "Thank you! Your form is submitted successfully.";
+
+  submittedPopup.classList.add("submitted-popup");
+
+  document.body.appendChild(submittedPopup);
+
+  setTimeout(() => {
+
+      submittedPopup.remove();
+
+      window.location.reload();
+
+  }, 3000);})
   .then(() => { window.location.reload(); })
   .catch(error => console.error('Error!', error.message))
-})
+});
