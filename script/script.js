dfd3c05d5a5a24c3e15c1ea378b2e843e6d7fbd8
BoudinDjama <48102683+BoudinDjama@users.noreply.github.com>
2024-02-03 10:47:05 -0500

    more tests
    testing if server will take it

--- script/script.js
+++ script/script.js
@@ -230,8 +230,7 @@ function sendEmail() {
 }
 
 //for contact form
-/*
-const scriptURL = "https://script.google.com/macros/s/AKfycbwkaeYBDZX_tJmgREX9ildcDSRARQynZ7nR6PJxkGxFFWkqGrgdExgUtagUFOlFAr94jQ/exec";
+/*const scriptURL = "https://script.google.com/macros/s/AKfycbwkaeYBDZX_tJmgREX9ildcDSRARQynZ7nR6PJxkGxFFWkqGrgdExgUtagUFOlFAr94jQ/exec";
 
 const form = document.getElementById("myForm");
 
@@ -284,3 +283,14 @@ form?.addEventListener("submit", async (e) => {
     }
 
 });*/
+const scriptURL = "https://script.google.com/macros/s/AKfycbwkaeYBDZX_tJmgREX9ildcDSRARQynZ7nR6PJxkGxFFWkqGrgdExgUtagUFOlFAr94jQ/exec";
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("Thank you! your form is submitted successfully." ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
